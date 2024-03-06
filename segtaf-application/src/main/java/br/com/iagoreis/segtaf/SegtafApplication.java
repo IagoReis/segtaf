3d3ad8e52f1a2ea2e550cc300a3d8d8aac1d99b4
Iago Reis <iagoidrs@gmail.com>
2024-03-06 20:05:31 -0300

    feat: projeto separado em modulos

--- segtaf-application/src/main/java/br/com/iagoreis/segtaf/SegtafApplication.java
+++ segtaf-application/src/main/java/br/com/iagoreis/segtaf/SegtafApplication.java
@@ -3,10 +3,11 @@ package br.com.iagoreis.segtaf;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackageClasses = SegtafApplication.class)
 public class SegtafApplication {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(SegtafApplication.class, args);
 	}
 
